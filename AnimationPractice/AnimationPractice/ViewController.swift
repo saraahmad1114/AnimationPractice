7f23e018a84e74dc4630c2ba2fa960c8022e17fd
Sara <saraahmad1114@yahoo.com>
2016-11-01 13:18:05 -0400

    chnages

--- AnimationPractice/AnimationPractice/ViewController.swift
+++ AnimationPractice/AnimationPractice/ViewController.swift
@@ -10,42 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var topTextfield: UITextField!
-    @IBOutlet weak var bottomLabel: UILabel!
-    @IBOutlet weak var belowLabel: UILabel!
-    @IBOutlet weak var centerLabel: UILabel!
+
+    @IBOutlet weak var bigRedBox: UIView!
+    
+    
+    //@IBOutlet weak var blueBox: UIView!
+    
+//    @IBOutlet weak var topTextfield: UITextField!
+//    @IBOutlet weak var bottomLabel: UILabel!
+//    @IBOutlet weak var belowLabel: UILabel!
+//    @IBOutlet weak var centerLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        view.removeConstraints(view.constraints)
+        bigRedBox.translatesAutoresizingMaskIntoConstraints = false
+        bigRedBox.centerXAnchor.constraintEqualToAnchor(self.view.centerXAnchor).active = true
+        bigRedBox.centerYAnchor.constraintEqualToAnchor(self.view.centerYAnchor).active = true
+        bigRedBox.widthAnchor.constraintEqualToAnchor(self.view.widthAnchor, multiplier: 0.10).active = true
+        bigRedBox.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 0.10).active = true
+        
+
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
-        
-        topTextfield.backgroundColor = UIColor.blueColor()
-        
+        //self.topTextfield.backgroundColor = UIColor.blueColor()
         //making the labels disappear 
-        UIView.animateWithDuration(2, animations: {
-            self.centerLabel.alpha = 2
-            }, completion: {
-                (value: Bool) in
-                self.centerLabel.hidden = true
-        })
+//        UIView.animateWithDuration(2, animations: {
+//            self.centerLabel.alpha = 2
+//            }, completion: {
+//                (value: Bool) in
+//                self.centerLabel.hidden = true
+//        })
+//        UIView.animateWithDuration(2, animations: {
+//            self.belowLabel.alpha = 2
+//            }, completion: {
+//                (value: Bool) in
+//                self.belowLabel.hidden = true
+//        })
+//        UIView.animateWithDuration(2, animations: {
+//            self.bottomLabel.alpha = 2 },
+//                completion: { (value: Bool) in
+//            self.bottomLabel.hidden = true
+//        })
+//        UIView.animateWithDuration(0.5, delay: 0.4,
+//        options: [], animations: {
+//        self.topTextfield.center.x += self.view.bounds.width
+//            }, completion: nil)
+//        
+//        UIView.animateWithDuration(0.5, delay: 0.4, options: [.Autoreverse], animations: {
+//            self.belowLabel.center.x += self.view.bounds.width
+//            }, completion: nil)
         
         
-        UIView.animateWithDuration(2, animations: {
-            self.belowLabel.alpha = 2
-            }, completion: {
-                (value: Bool) in
-                self.belowLabel.hidden = true
-        })
-        
-        UIView.animateWithDuration(2, animations: {
-            self.bottomLabel.alpha = 2 },
-                completion: { (value: Bool) in
-            self.bottomLabel.hidden = true
-        })
-        
-        UIView.animateWithDuration(0.5, delay: 0.4,
-        options: .Repeat, animations: {
-        self.topTextfield.center.x += self.view.bounds.width
-            }, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
